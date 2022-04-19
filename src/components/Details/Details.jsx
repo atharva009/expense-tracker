84e88d1f21bea64d34962812f5c4a00d3c5cc0df
atharva009 <atharvahankare@gmail.com>
2022-04-19 22:05:15 +0530

    speechly api added

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,21 +1,25 @@
-import React from "react";
-import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
-import { Doughnut } from "react-chartjs-2";
+import React from 'react';
+import { ArcElement } from "chart.js";
+import Chart from "chart.js/auto";
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
 
-import useStyles from "./styles";
+import useStyles from './styles';
+import useTransactions from '../../useTransactions';
 
-const Details = ({ title }) => {
-    const classes = useStyles();
+const DetailsCard = ({ title, subheader }) => {
+  const { total, chartData } = useTransactions(title);
+  const classes = useStyles();
 
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
-      <CardHeader title={title} />
+      <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <Typography variant="h5">₹50</Typography>
-        {/* <Doughnut data="DATA" /> */}
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
 };
 
-export default Details;
+export default DetailsCard;
--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,35 +1,35 @@
 import React, { useContext } from 'react';
-import { Avatar, IconButton, List as MUIList, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, Slide } from '@material-ui/core';
+import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
 
-import useStyles from './styles';
 import { ExpenseTrackerContext } from '../../../context/context';
+import useStyles from './styles';
 
 const List = () => {
-    const classes = useStyles();
-    const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
-    
+  const classes = useStyles();
+  const { transactions, deleteTransaction } = useContext(ExpenseTrackerContext);
+
   return (
     <MUIList dense={false} className={classes.list}>
-        {transactions.map((transaction) => (
-            <Slide direction='down' in mountOnEnter unmountOnExit key={transaction.id}>
-                <ListItem>
-                    <ListItemAvatar>
-                        <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
-                            <MoneyOff />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={transaction.category} secondary={`₹${transaction.amount} - ${transaction.date}`} />
-                    <ListItemSecondaryAction>
-                        <IconButton edge="end" aria-label='delete' onClick={() => deleteTransaction(transaction.id)}>
-                            <Delete />
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>
-            </Slide>
-        ))}
+      {transactions.map((transaction) => (
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
     </MUIList>
-  )
-}
+  );
+};
 
 export default List;
